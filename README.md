e6bc923c0b752358deb469ef8b8c03d2fc24ba7a
Yuuichi Akagawa <y-akagawa@ammlab.org>
2012-02-21 22:53:39 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+HelloADK
+========
+HelloADK based on yanzm's HelloADK.
+This project work with STM32F4_ADK.
+
+License
+-------
+Copyright &copy; 2012 Yuuichi Akagawa
+Licensed under the [Apache License, Version 2.0][Apache]
+
+[Apache]: http://www.apache.org/licenses/LICENSE-2.0
+
---
+++ src/org/ammlab/android/helloadk/HelloADKActivity.java
@@ -0,0 +1,319 @@
+/*
+ * Copyright (C) 2012 Yuuichi Akagawa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ammlab.android.helloadk;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.android.future.usb.UsbAccessory;
+import com.android.future.usb.UsbManager;
+
+import android.app.*;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class HelloADKActivity extends Activity implements Runnable {
+
+    private static final String TAG = "HelloADK";
+
+    private static final String ACTION_USB_PERMISSION = "org.ammlab.android.app.helloadk.action.USB_PERMISSION";
+
+    private PendingIntent mPermissionIntent;
+    private boolean mPermissionRequestPending;
+
+    private UsbManager mUsbManager;
+    private UsbAccessory mAccessory;
+
+    ParcelFileDescriptor mFileDescriptor;
+
+    FileInputStream mInputStream;
+    FileOutputStream mOutputStream;
+    
+    private ToggleButton mToggleButton;
+    private ToggleButton mBtnStatusButton;
+    private TextView mStatusView;
+
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    // Intent からアクセサリを取得
+                    UsbAccessory accessory = UsbManager.getAccessory(intent);
+
+                    // パーミッションがあるかチェック
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        // 接続を開く
+                        openAccessory(accessory);
+                    } else {
+                        Log.d(TAG, "permission denied for accessory " + accessory);
+                    }
+                    mPermissionRequestPending = false;
+                }
+            } else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
+                // Intent からアクセサリを取得
+                UsbAccessory accessory = UsbManager.getAccessory(intent);
+                if (accessory != null && accessory.equals(mAccessory)) {
+                    // 接続を閉じる
+                    closeAccessory();
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // UsbManager のインスタンスを取得
+        mUsbManager = UsbManager.getInstance(this);
+
+        // オレオレパーミッション用 Broadcast Intent
+        mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+
+        // オレオレパーミッション Intent とアクセサリが取り外されたときの Intent を登録
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+        registerReceiver(mUsbReceiver, filter);
+
+        setContentView(R.layout.main);
+        
+        mToggleButton = (ToggleButton) findViewById(R.id.toggleBtn);
+        mStatusView = (TextView) findViewById(R.id.status);
+        mBtnStatusButton= (ToggleButton) findViewById(R.id.btnstatusBtn);
+        
+        //ボタンを押したときのハンドラ(アクセサリのLEDスイッチ)
+        mToggleButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                byte command = 0x1;
+                byte value = (byte) (isChecked ? 0x1 : 0x0);
+                sendCommand(command, value);
+            }
+        });
+
+        enableControls(false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mInputStream != null && mOutputStream != null) {
+            return;
+        }
+
+        // USB Accessory の一覧を取得
+        UsbAccessory[] accessories = mUsbManager.getAccessoryList();
+        UsbAccessory accessory = (accessories == null ? null : accessories[0]);
+        if (accessory != null) {
+            // Accessory にアクセスする権限があるかチェック
+            if (mUsbManager.hasPermission(accessory)) {
+                // 接続を開く
+                openAccessory(accessory);
+            } else {
+                synchronized (mUsbReceiver) {
+                    if (!mPermissionRequestPending) {
+                        // パーミッションを依頼
+                        mUsbManager.requestPermission(accessory, mPermissionIntent);
+                        mPermissionRequestPending = true;
+                    }
+                }
+            }
+        } else {
+            Log.d(TAG, "mAccessory is null");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        closeAccessory();
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(mUsbReceiver);
+        super.onDestroy();
+    }
+
+    private void openAccessory(UsbAccessory accessory) {
+        // アクセサリにアクセスするためのファイルディスクリプタを取得
+        mFileDescriptor = mUsbManager.openAccessory(accessory);
+
+        if (mFileDescriptor != null) {
+            mAccessory = accessory;
+            FileDescriptor fd = mFileDescriptor.getFileDescriptor();
+
+            // 入出力用のストリームを確保
+            mInputStream = new FileInputStream(fd);
+            mOutputStream = new FileOutputStream(fd);
+
+            // この中でアクセサリとやりとりする
+            Thread thread = new Thread(null, this, "DemoKit");
+            thread.start();
+            Log.d(TAG, "accessory opened");
+
+            enableControls(true);
+        } else {
+            Log.d(TAG, "accessory open fail");
+        }
+    }
+
+    private void closeAccessory() {
+        enableControls(false);
+
+        try {
+            if (mFileDescriptor != null) {
+                mFileDescriptor.close();
+            }
+        } catch (IOException e) {
+        } finally {
+            mFileDescriptor = null;
+            mAccessory = null;
+        }
+    }
+
+    private void enableControls(boolean enable) {
+        if (enable) {
+            mStatusView.setText("connected");
+        } else {
+            mStatusView.setText("not connected");
+        }
+        mToggleButton.setEnabled(enable);
+    }
+
+    private static final int MESSAGE_LED = 1;
+
+    private class LedMsg {
+        private byte on;
+
+        public LedMsg(byte on) {
+            this.on = on;
+        }
+
+        public boolean isOn() {
+            if(on == 0x1)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    // ここでアクセサリと通信する
+    @Override
+    public void run() {
+        int ret = 0;
+        byte[] buffer = new byte[16384];
+        int i;
+
+        // アクセサリ -> アプリ
+        while (ret >= 0) {
+            try {
+                ret = mInputStream.read(buffer);
+            } catch (IOException e) {
+                break;
+            }
+
+            if( ret > 0 ){
+                Log.d(TAG, ret + " bytes message received.");
+            }
+            i = 0;
+            while (i < ret) {
+                int len = ret - i;
+
+                switch (buffer[i]) {
+                    case 0x1:
+                        // 2byte のオレオレプロトコル
+                        // 0x1 0x0 や 0x1 0x1 など
+                        if (len >= 2) {
+                            Message m = Message.obtain(mHandler, MESSAGE_LED);
+                            m.obj = new LedMsg(buffer[i + 1]);
+                            mHandler.sendMessage(m);
+                            i += 2;
+                        }
+                        break;
+
+                    default:
+                        Log.d(TAG, "unknown msg: " + buffer[i]);
+                        i = len;
+                        break;
+                }
+            }
+
+        }
+    }
+
+    // UI スレッドで画面上の表示を変更
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_LED:
+                    LedMsg o = (LedMsg) msg.obj;
+                    handleLedMessage(o);
+                    break;
+            }
+        }
+    };
+
+    private void handleLedMessage(LedMsg l) {
+        if(l.isOn()) {
+        	mBtnStatusButton.setChecked(true);
+        }
+        else {
+        	mBtnStatusButton.setChecked(false);
+        }
+    }
+
+    // アプリ -> アクセサリ
+    public void sendCommand(byte command, byte value) {
+        byte[] buffer = new byte[2];
+        
+        if(value != 0x1 && value != 0x0)
+            value = 0x0;
+
+        // 2byte のオレオレプロトコル
+        // 0x1 0x0 や 0x1 0x1
+        buffer[0] = command;
+        buffer[1] = value;
+        if (mOutputStream != null) {
+            try {
+                mOutputStream.write(buffer);
+            } catch (IOException e) {
+                Log.e(TAG, "write failed", e);
+            }
+        }
+    }
+}
